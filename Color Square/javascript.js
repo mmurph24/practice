3286e30ffb8fa82d8bd1af9a34b22e4ba6b783c9
Murphy <mmurph24@users.noreply.github.com>
2017-05-09 23:27:36 -0400

    Add files via upload

---
+++ Color Square/javascript.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+    var red = "#CF000F";
+    var blue = "#446CB3";
+    var green = "#26A65B";
+    var orange = "#E87E04";
+    var color = 0;
+
+    $("#red").click(function(){
+      color = red;
+    });
+
+    $("#blue").click(function(){
+      color = blue;
+    });
+
+    $("#green").click(function(){
+      color = green;
+    });
+
+    $("#orange").click(function(){
+      color = orange;
+    });
+
+    $(".grid-item").hover(function(){
+      $(this).css("background-color",color);
+
+    });
+
+
+});
