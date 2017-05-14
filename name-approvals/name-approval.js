5a004320c04b75b3fdb8cf72a8ae113f954d6caf
Murphy <mmurph24@users.noreply.github.com>
2017-05-14 11:23:15 -0400

    Update name-approval.js

--- name-approvals/name-approval.js
+++ name-approvals/name-approval.js
@@ -2,34 +2,41 @@ $(document).ready(function(){
 
   var isApproved = false;
 
+  // The 10 most common boys names of the past 100 years
+
   var approvedBoyNames = ["james", "john", "robert", "michael", "william", "david",
   "richard", "joseph", "thomas", "charles"];
+
+  // The 10 most common girls names of the past 100 years
 
   var approvedGirlNames = ["mary", "patricia", "jennifer", "elizabeth", "linda", "barbara",
   "susan", "jessica", "margaret", "sarah"];
 
   var approvedNames = [approvedBoyNames, approvedGirlNames];
 
+  // Check user input against list of approved names
+
   function checkName (nameList) {
     var userName = $('#nameField').val().toLowerCase();
 
     for (var i = 0; i < nameList.length; i++) {
       for (var j = 0; j < nameList[i].length; j++) {
-        if (userName === nameList[j]) {
+        if (userName === nameList[i][j]) {
           isApproved = true;
-        } else {
-          alert(nameList[i]); // for debugging
         }
       }
     }
     if (isApproved === true) {
       $("#results-span").text($('#nameField').val() + " is acceptable.");
+      isApproved = false;
     } else {
       $("#results-span").text($('#nameField').val() + " is not approved.");
     }
   }
 
+  // Run check on form submission
+
   $("#name-submit").click(function() {
-    checkName(approvedNames);
+      checkName(approvedNames);
   });
 });
